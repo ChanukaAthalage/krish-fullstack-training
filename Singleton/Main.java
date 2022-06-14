b4bd5b0ac1f527abeebdf7a0ee4e22c1878a198a
ChanukaAthalage <53206339+ChanukaAthalage@users.noreply.github.com>
2022-06-14 10:56:03 +0530

    Singleton example

---
+++ Singleton/Main.java
@@ -0,0 +1,17 @@
+package singleton;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		SingletonExam student = SingletonExam.getExam();
+		//SingletonExam singletonExam1 = SingletonExam.getExam();
+		
+		System.out.println(student);
+		//System.out.println(singletonExam1);
+		
+
+	}
+
+}
